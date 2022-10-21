199162f1cf784f4823cf1ba4d0b5be3422d4afff
KwaziCoder <63724691+KwaziCoder@users.noreply.github.com>
2022-10-21 22:53:57 +0300

    circular menu training

--- js/index.js
+++ js/index.js
@@ -1,93 +1,40 @@
-const player = document.querySelector(".player"),
-      title = document.querySelector(".title"),
-      playBtn = document.querySelector(".play"),
-      nextBtn = document.querySelector(".next"),
-      prevBtn = document.querySelector(".prev"),
-      audio = document.querySelector(".audio"),
-      progressContainer = document.querySelector(".progress__container"),
-      progress = document.querySelector(".progress"),
-      cover = document.querySelector(".cover__image"),
-      imgSrc = document.querySelector(".img__src");
+const menus = [...document.querySelectorAll(".circular-menu")];
 
-
+menus.map((menu) => {
+    const items = menu.querySelectorAll(".circular-menu__item");
+    const button = menu.querySelector(".circual-menu__button")
     
-const songs = ["Let it go", "Its raining men", "Call me maybe"];
 
-let songIndex = 0;
-let isPlaying = false;
+    let active = false;
+    const length = items.length;
+    const arc = 2 * Math.PI * (1 / length);
+    const radius = 40;
 
-function initPlayer() {
-    loadSong(songIndex);
-}
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        
 
-function loadSong(songIndex) {
-    title.textContent = songs[songIndex];
-    audio.src = `audio/${songs[songIndex]}.mp3`;
-    cover.src = `img/${songs[songIndex]}.svg`;
+        active = !active;
 
-}
+        if (active) {
+            button.classList.add("circual-menu__button_active");
 
-function playPauseSong() {
-    if (isPlaying) {
-        audio.pause();
-        isPlaying = false;
-        imgSrc.src = "img/play.svg";
-        cover.classList.remove("active");
-    } 
-    else {
-        audio.play();
-        isPlaying = true;
-        imgSrc.src = "img/pause.svg";
-        cover.classList.add("active");
-    }
-    
-}
+            for(let i = 0; i < length; i++) {
+                const angle = i * arc;
+                const x = radius * Math.cos(angle);
+                const y = radius * Math.sin(angle);
 
-function forcePlaySong() {
-    audio.play();
-    isPlaying = true;
-    imgSrc.src = "img/pause.svg";
-    cover.classList.add("active");
-}
+                items[i].style.left = 50 + x + "%";
+                items[i].style.top = 50 + y + "%";
+            }
+        }
 
-function changeSong(newIndex) {
-    if (newIndex === songs.length || newIndex === -1) return;
-    songIndex = newIndex;
-    loadSong(songIndex);
-    forcePlaySong();
-}
+        else {
+            button.classList.remove("circual-menu__button_active");
 
-function updateProgress(e) {
-    const {duration, currentTime} = e.srcElement;
-    const progressPersentage = (currentTime / duration) * 100;
-    progress.style.width = `${progressPersentage}%`;
-}
-
-function setProggress(e) {
-    const width = this.clientWidth;
-    const clickX = e.offsetX;
-    const progressPersentage = (clickX / width) * 100;
-    const duration = audio.duration;
-
-    audio.currentTime = progressPersentage / 100 * duration;
-    progress.style.width = `${progressPersentage}%`;
-    
-}
-
-initPlayer();
-
-playBtn.addEventListener("click", playPauseSong);
-nextBtn.addEventListener("click", () => {
-    changeSong(songIndex + 1);
+            for(let i = 0; i < length; i++) {
+                items[i].removeAttribute("style");
+            }
+        }
+    });
 });
-prevBtn.addEventListener("click", () => {
-    changeSong(songIndex - 1);
-});
-audio.addEventListener("timeupdate", updateProgress)
-progressContainer.addEventListener("click", setProggress)
-audio.addEventListener("ended", () => {
-    if (songs.length === songIndex + 1) {
-        cover.classList.remove("active");
-    }
-    changeSong(songIndex + 1);
-})
